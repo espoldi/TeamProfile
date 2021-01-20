03159ae1aa0a697d68923379509d10a1e9611c88
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-19 21:54:54 -0500

    fixed role questions

--- app.js
+++ app.js
@@ -15,74 +15,72 @@ const render = require("./lib/htmlRenderer");
 var teamMates = [];
 
 const generic = [
-        {
-            type: 'rawlist',
-            name: 'role',
-            message: 'What is your role?',
-            choices: ['Manager', 'Engineer', 'Intern']
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is your ID number?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?'
-        }
-    ];
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is your role?',
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
-const promptUser = () => {return inquirer.prompt(generic)};
+const promptUser = (questions) => { return inquirer.prompt(questions) };
 
-
-switch (response.role) {
-    case 'Manager':
-        return inquirer.prompt([
-            {
+function differentiate(role) {
+    switch (role) {
+        case 'Manager':
+            return {
                 type: 'input',
                 name: 'officeNumber',
                 message: 'What is your office number?'
             }
-        ])
-    case 'Engineer':
-        return inquirer.prompt([
-            {
+        case 'Engineer':
+            return {
                 type: 'input',
                 name: 'github',
                 message: 'What is your github username?'
             }
-        ])
-    case 'Intern':
-        return inquirer.prompt([
-            {
+        case 'Intern':
+            return {
                 type: 'input',
                 name: 'school',
                 message: 'What is the name of your school?'
             }
-        ])
     }
+}
 
 // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
 
 function writeToFile(fileName, data) {
-    writeFileAsync(fileName, data);
-    console.log("test successful");
+    fs.writeFile(fileName, data, () => {
+        console.log("wrote successful");
+    });
+
 }
 
 // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
 
 const init = async () => {
     const answers = await promptUser();
-    const page = await htmlRenderer(answers);
+    const page = await render(teamMates);
     const html = writeToFile('team.html', page);
 }
 
+init();
 // Hint: you may need to check if the `output` folder exists and create it if it does not.
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different information; write your code to ask different questions via inquirer depending on employee type.
